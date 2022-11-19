d60684c066d7ed52ca81446d3d0dd777130baa03
PieceOfEon <danilskripka1996@gmail.com>
2022-11-19 13:45:38 +0200

    Add project files.

---
+++ Classes/PlatformController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTestJump.Classes
+{
+    public static class PlatformController
+    {
+        public static List<platform> platforms;
+        public static int startPlatformPosY = 400;
+        public static int score = 0;
+
+        public static void AddPlatform(PointF position)
+        {
+            platform platform = new platform(position);
+            platforms.Add(platform);
+            
+            
+        }
+
+        public static void GenerateStartSequence()
+        {
+            Random r = new Random();
+            for(int i=0; i<10;i++)
+            {
+                int x = r.Next(0, 270);
+                int y = r.Next(30, 40);
+                startPlatformPosY -= y;
+                PointF position = new PointF(x,startPlatformPosY);
+                platform platform = new platform(position);
+                platforms.Add(platform);
+            }
+        }
+
+        public static void GenerateRandomPlatform()
+        {
+            Random r = new Random();
+            int x = r.Next(0, 270);
+            PointF position = new PointF(x, startPlatformPosY);
+            platform platform = new platform(position);
+            platforms.Add(platform);
+            
+
+        }
+
+        public static void ClearPlatforms()
+        {
+            for(int i = 0; i<platforms.Count;i++)
+            {
+                if (platforms[i].transform.position.Y>=700)
+                {
+                    platforms.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
---
+++ Classes/platform.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTestJump.Classes
+{
+    public class platform
+    {
+        Image sprite;
+        public transform transform;
+        public int sizeX;
+        public int sizeY;
+        public bool isTouchedByPlayer;
+
+        public platform(PointF pos)
+        {
+            sprite = Properties.Resources.ButtonExtraWide_Pink;
+            sizeX = 125;
+            sizeY = 15;
+            transform = new transform(pos, new Size(sizeX, sizeY));
+            isTouchedByPlayer = false;
+
+        }
+
+        public void DrawSprite(Graphics g)
+        {
+            g.DrawImage(sprite, transform.position.X, transform.position.Y, transform.size.Width, transform.size.Height);
+        }
+    }
+}
